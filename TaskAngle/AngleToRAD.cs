829115086d80c06038df43bbc19e2f6e6ed501a8
LarisaChAng <LChS81@mail.ru>
2022-11-06 15:14:39 +0800

    Добавьте файлы проекта.

---
+++ TaskAngle/AngleToRAD.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAngle
+
+{
+    class AngleToRAD
+    {
+        int g;
+        int m;
+        int s;
+
+        
+        public int G
+        {
+            get
+            {                            
+                return g;                               
+            }
+            set
+            {                
+                if (g>359)
+                {                    
+                    g = 360;
+                }
+                else if (g < -359)
+                {                    
+                    g = -360;
+                }
+                else
+                {                    
+                    g = value;
+                }                
+            }
+        }
+        public int M
+        {
+            get
+            {
+                return m;
+            }
+            set
+            {
+                G += value / 60;
+                m = value % 60;
+            }
+        }
+        public int S
+        {
+            get
+            {
+                return s;
+            }
+            set
+            {
+                M += value / 60;
+                s = value % 60;
+            }
+        }
+
+        public AngleToRAD(int g, int m, int s)
+        {
+            this.G = g;
+            this.M = m;
+            this.S = s;
+        }
+
+        public int ToGRad()
+        {
+            //sG - сумма градусов из исходных данных
+            int sG = g + m / 60 + s / 3600;
+            return sG;
+        }
+        public double ToRad()
+        {            
+            double sG = (double)g + (double)m / 60 + (double)s / 3600;            
+            double gR = sG * Math.PI / 180;
+            return gR;
+        }
+    }
+}
---
+++ TaskAngle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAngle
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Значение угла из градусов в радианы");
+            Console.WriteLine("Введите последовательно градусы-минуты-секунды УГЛА (в целых числах):");
+            int g = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            AngleToRAD anGrR = new AngleToRAD (g, m, s);
+            
+            double totalRad = anGrR.ToRad();            
+            int totalGRad = anGrR.ToGRad();
+
+            Console.WriteLine("Значение угла в радианах {0:0.000}, а в градусах {1}", totalRad, totalGRad);
+            Console.ReadKey();
+        }        
+    }
+}
